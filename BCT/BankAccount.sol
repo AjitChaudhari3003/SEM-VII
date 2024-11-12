7dd365b6f59049f7e242bee66d0264edd7644c88
Ajit Chaudhari <114723319+AjitChaudhari3003@users.noreply.github.com>
2024-11-12 08:30:21 +0530

    Update BankAccount.sol

--- BCT/BankAccount.sol
+++ BCT/BankAccount.sol
@@ -1,47 +1,29 @@
-// Question  - Write a smart contract on a test network, for Bank account of a customer for following operations:
-// Deposit money
-// Withdraw Money
-// Show balance
-
-
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract SimpleBank {
-    address public owner;
-    mapping(address => uint256) private balances;
+contract Bank{
+    mapping (address => uint256) private balances;
 
-    event Deposit(address indexed depositor, uint256 amount);
-    event Withdrawal(address indexed withdrawer, uint256 amount);
+    event Deposite( address depositer , uint amount);
+    event Withdreal( address withdrewal , uint amount);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "You are not the owner");
-        _;
-    }
+  // deposit money into bank
+  function deposit() public payable {
+    require(msg.value >0 ,"Deposite money must be greater than zero");
+    balances[msg.sender] += msg.value;
+    emit Deposite(msg.sender, msg.value);
+  }
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    function deposit() public payable {
-        require(msg.value > 0, "Deposit amount must be greater than zero");
-        balances[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
-    }
-
-    function withdraw(uint256 amount) public {
-        require(amount <= balances[msg.sender], "Insufficient funds");
-        balances[msg.sender] -= amount;
-        payable(msg.sender).transfer(amount);
-        emit Withdrawal(msg.sender, amount);
-    }
-
-    function getBalance() public view returns (uint256) {
-        return balances[msg.sender];
-    }
-
-    function checkTotalBalance() public view onlyOwner returns (uint256) {
-        return address(this).balance;
-    }
+  //withdrewal money from bank
+  function withdrw(uint256 amount) public {
+    require(amount < balances[msg.sender] ," Insufficient balance");
+    balances[msg.sender] -= amount;
+    payable(msg.sender).transfer(amount);
+    emit Withdreal(msg.sender , amount);
+   
+  }
+  // getbalance
+  function getBalance() public view returns (uint256){
+    return balances[msg.sender];
+  }
 }
-
